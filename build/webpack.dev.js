2d2820ee8622b5f5edc1d57cb67c03942988188e
大宝 <742393511@qq.com>
2021-07-17 10:31:12 +0800

    feat: use npm add react react-dom && add webpack

---
+++ build/webpack.dev.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports={
+  mode:'development',
+  entry:'./src/main.js',
+  output:{
+    filename:'index.bundle.js',
+    path:path.resolve(__dirname,'dist')
+  },
+  module:{
+    rules:[
+      {
+        test:/\.js$/i,
+        use:'babel-loader',
+        exclude: /(node_modules)/
+        // include:['src']
+        // exclude:['node_modules']
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'./public/index.html'
+    })
+  ]
+}
--- src/hello_world.js
+++
@@ -1,6 +0,0 @@
-class App extends React.Component{
-  render(){
-    return <h2>hello world</h2>
-  }
-}
-ReactDOM.render(<App />, document.getElementById('root'))
